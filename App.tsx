6ef51b31477f50c66754642567a5fd87237e9504
kakizawaayato <chibairodori25@gmail.com>
2023-11-25 19:14:02 +0900

    ✨クリップ記事の永続化

--- App.tsx
+++ App.tsx
@@ -2,8 +2,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { tabIconOption } from './settings/tabIconOption';
-import { store } from './store';
+import { persistor, store } from './store';
 import { ClipTab, HomeTab } from './tabs';
 
 export default function App() {
@@ -11,12 +12,14 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Tab.Navigator screenOptions={tabIconOption}>
-          <Tab.Screen name="HomeTab" component={HomeTab} options={{ headerShown: false }} />
-          <Tab.Screen name="ClipTab" component={ClipTab} options={{ headerShown: false }} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={tabIconOption}>
+            <Tab.Screen name="HomeTab" component={HomeTab} options={{ headerShown: false }} />
+            <Tab.Screen name="ClipTab" component={ClipTab} options={{ headerShown: false }} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- store/index.ts
+++ store/index.ts
@@ -1,8 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { PERSIST, persistReducer, persistStore } from 'redux-persist';
 import userReducer from './userSlice';
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [PERSIST],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
